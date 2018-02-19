910b00fe6e6dd8467c327d9d9b8251cf5640d8af
Mikko Marttila <mikmart@mikkomarttila.com>
2018-02-19 15:12:31 +0200

    Use package name in api_key option

--- R/fmi_query.R
+++ R/fmi_query.R
@@ -6,11 +6,13 @@
 #'
 #' @param api_key A length 1 character vector containing your personal FMI API
 #'   key required to access the download service.
+#' @seealso \href{https://en.ilmatieteenlaitos.fi/open-data}{FMI Open Data website}
+#'   for obtaining a new API key.
 #' @export
 fmi_set_key <- function(api_key)
 {
   stopifnot(is.character(api_key), length(api_key) == 1)
-  options(fmi.api_key = api_key)
+  options(fmir.api_key = api_key)
 }
 
 
@@ -40,7 +42,7 @@ fmi_set_key <- function(api_key)
 #' @seealso \code{\link{fmi_set_key}} for setting the API key for your session.
 #' @export
 fmi_query <- function(type = c("real-time", "daily", "monthly"),
-                      ..., api_key = getOption("fmi.api_key"))
+                      ..., api_key = getOption("fmir.api_key"))
 {
   stored_query <- fmi_stored_query(type)
   xml_url <- fmi_xml_url(stored_query, api_key)
