6967f5327f1462f19ad2ac18e866f972363cde32
Mikko Marttila <mikko.marttila.fi@gmail.com>
2018-02-19 23:57:31 +0200

    Add tests for query parameter construction

---
+++ tests/testthat/test-query_params.R
@@ -0,0 +1,24 @@
+library(fmir)
+context("Query parameters")
+
+test_that("unnamed query parameters throw error", {
+  expect_error(fmi_query_params(list()))
+  expect_error(fmi_query_params(NULL))
+  expect_error(fmi_query_params(NA_character_))
+  expect_error(fmi_query_params(character(0)))
+  expect_error(fmi_query_params(list(1)))
+  expect_error(fmi_query_params(list(a = 1, 2)))
+})
+
+test_that("length != 1 query parameters throw error", {
+  expect_error(fmi_query_params(list(a = 1:10)))
+  expect_error(fmi_query_params(list(a = 1, b = letters)))
+  expect_error(fmi_query_params(list(a = 1, b = character(0))))
+})
+
+test_that("query parameters are concatenated correctly", {
+  expect_equal(fmi_query_params(list(a = 1)), "a=1")
+  expect_equal(fmi_query_params(list(a = 1, b = 2)), "a=1&b=2")
+  expect_equal(fmi_query_params(list(a = 1, b = 2, c = 3)), "a=1&b=2&c=3")
+  expect_equal(fmi_query_params(list(a = 1, a = 2)), "a=1&a=2")
+})
