1231aafac8cdb8f4c6a9c1f16c48da20a5423a66
Mikko Marttila <mikko.marttila.fi@gmail.com>
2018-02-19 21:39:20 +0200

    Update and document sample data

--- data-raw/ouludaily10.R
+++ data-raw/ouludaily10.R
@@ -1,6 +1,7 @@
 library(lubridate)
 library(purrr)
-library(fmir)
+
+devtools::load_all()
 
 # My API key: not pushed to GitHub
 fmi_set_key(readLines("data-raw/api-key.txt"))
@@ -9,7 +10,9 @@ years <- 2010:2017
 
 ouludaily10 <- map_df(years, function(y) {
   s <- make_date(y)
-  e <- s + years(1)
+  e <- s + years(1) - days(1)
   q <- fmi_query("daily", place = "Oulu", starttime = s, endtime = e)
   fmi_data(q)
 })
+
+devtools::use_data(ouludaily10, overwrite = TRUE)
