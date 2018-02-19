c0e8b5e6c4413c4e7331d1b55b864f09036de702
Mikko Marttila <mikko.marttila.fi@gmail.com>
2018-02-19 23:42:16 +0200

    Add API key validation tests

---
+++ tests/testthat/test-api_key.R
@@ -0,0 +1,14 @@
+library(fmir)
+context("API key validation")
+
+test_that("null API key warns and returns dummy", {
+  expect_warning(validate_api_key(NULL))
+  expect_is(validate_api_key(NULL), "character")
+})
+
+test_that("non-null API key validation works", {
+  expect_error(validate_api_key(1))
+  expect_error(validate_api_key(mtcars))
+  expect_error(validate_api_key(letters))
+  expect_error(validate_api_key(NA_character_))
+})
