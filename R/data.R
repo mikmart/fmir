a02501167795b2f8e3748bdc0cf2f91cdb563617
Mikko Marttila <mikmart@mikkomarttila.com>
2018-02-20 19:10:14 +0200

    Update data documentation

--- R/data.R
+++ R/data.R
@@ -7,13 +7,13 @@
 #' @format A data frame with 2922 rows and 8 variables:
 #' \describe{
 #'   \item{location}{coordinates of the observation station}
-#'   \item{time}{date of observation, UTC + 2}
+#'   \item{time}{date of observation}
 #'   \item{rrday}{precipitation rate}
 #'   \item{snow}{snow depth}
-#'   \item{tday}{average temperature of the day, degrees Celcius}
+#'   \item{tday}{average temperature, degrees Celcius}
 #'   \item{tg_pt12h_min}{?}
-#'   \item{tmax}{maximum temperature of the day, degrees Celcius}
-#'   \item{tmin}{minimum temperature of the day, degrees Celcius}
+#'   \item{tmax}{maximum temperature, degrees Celcius}
+#'   \item{tmin}{minimum temperature, degrees Celcius}
 #' }
 #'
 "ouludaily10"
