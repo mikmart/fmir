d4d0e181711a2e7eb877c3376eb8bdc2cc653310
Mikko Marttila <mikmart@mikkomarttila.com>
2018-02-20 22:14:57 +0200

    Add list squashing function

--- R/utils.R
+++ R/utils.R
@@ -8,3 +8,23 @@ fmi_format_date <- function(x)
   stopifnot(is_dateish(x))
   format(x, format = "%Y-%m-%dT%H:%M:%SZ")
 }
+
+#' Turn a list into a character vector
+#'
+#' Use to turn a (possibly nested) list to a character vector. Names for the
+#' output vector are taken from the names of the top level list.
+#'
+#' @param x list
+#' @value named character vector
+#' @importFrom purrr %||%
+squash_list <- function(x) {
+  x <- purrr::map_if(x, purrr::is_bare_list, squash_list)
+
+  nm <- names(x) %||% ""
+  nm <- rep(names(x), lengths(x))
+
+  x <- purrr::map(x, as.character)
+  x <- purrr::flatten(x)
+
+  purrr::set_names(x, nm)
+}
