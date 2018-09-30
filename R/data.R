a7c25a83bfb87b6ab28151aeb3a1ff45ae5bf382
Mikko Marttila <mikmart@mikkomarttila.com>
2018-09-30 11:57:09 +0300

    Update ouludaily10 data with new features

--- R/data.R
+++ R/data.R
@@ -1,11 +1,14 @@
 #' Daily weather in Oulu, Finland, in 2010-2017
 #'
 #' A dataset downloaded from the Finnish Meteorological Institute's open data
-#' API using `fmir`. Contains daily simple weather observations from Oulu,
-#' Finland, covering the years 2010 to 2017.
+#' API using **fmir**. Contains daily simple weather observations from Oulu,
+#' Finland, covering the years 2010 to 2017. The data are made available by the
+#' [Finnish Meteorological Institute](https://en.ilmatieteenlaitos.fi) and are
+#' licensed under [CC-BY 4.0](https://creativecommons.org/licenses/by/4.0/).
 #'
 #' @format A data frame with 2922 rows and 8 variables:
 #' \describe{
+#'   \item{place}{city name}
 #'   \item{location}{coordinates of the observation station}
 #'   \item{time}{date of observation}
 #'   \item{rrday}{precipitation rate}
--- data-raw/ouludaily10.R
+++ data-raw/ouludaily10.R
@@ -1,18 +1,12 @@
-library(lubridate)
-library(purrr)
+library(fmir)
 
-devtools::load_all()
+# API key comes form envvar
+q <- fmi_query("daily",
+  place     = "Oulu",
+  starttime = "2010-01-01",
+  endtime   = "2017-12-31"
+)
 
-# My API key: not pushed to GitHub
-fmi_set_key(readLines("data-raw/api-key.txt"))
-
-years <- 2010:2017
-
-ouludaily10 <- map_df(years, function(y) {
-  s <- make_date(y)
-  e <- s + years(1) - days(1)
-  q <- fmi_query("daily", place = "Oulu", starttime = s, endtime = e)
-  fmi_data(q)
-})
+ouludaily10 <- fmi_data(q)
 
 devtools::use_data(ouludaily10, overwrite = TRUE)
