f4095c18cd6789f5fecd7ad844ced31c36775bdd
Mikko Marttila <mikmart@mikkomarttila.com>
2019-02-09 17:56:00 +0200

    Use a fixed end time in tests
    Test query was spanning a longer interval than when initially created,
    causing the test to fail.

--- tests/testthat/test-split_long_query.R
+++ tests/testthat/test-split_long_query.R
@@ -7,6 +7,10 @@ test_that("can identify long queries", {
   q <- fmi_query(endtime = lubridate::make_date(2017))
   expect_false(is_long_query(q))
 
-  q <- fmi_query("daily", starttime = lubridate::make_date(2017))
+  q <- fmi_query("daily",
+    starttime = lubridate::make_date(2017),
+    endtime = lubridate::make_date(2018, 12, 31)
+  )
+
   expect_length(fmi_split_long_query(q), 2)
 })
